f5ccfea2951a8e11b0da821217b00d2f890be03f
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-06-14 16:32:25 +0900

    直しました。いろいろ変えたよ。 #2

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,5 +1,5 @@
 import random
-
+import datetime
 def main():
     seikai = shutudai()
     kaitou(seikai)
@@ -15,12 +15,16 @@ def shutudai():
     return qas[r]["a"]
 
 def kaitou(seikai):
+    st = datetime.datetime.now()
     ans = input("答えるんだ:")
     if ans in seikai:
         print("正解！")
+        ed = datetime.datetime.now()
+        print((ed-st).seconds)
     else:
         print("出直してこい！！")
-    
+        ed = datetime.datetime.now()
+        print(f"{(ed-st).seconds}秒")
 if __name__ == "__main__":
     main() 
 
