df9cc2cc88dd6bbccc9db697134d900d4bdb143e
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-06-28 14:55:00 +0900

    7番まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,7 +8,26 @@ def key_down(event):
 def key_up(event):
     global key
     key = ""
-    
+
+def main_proc():    #()の中にeventを入れるのはバインドするときのみ。
+    global cx, cy #keyはglobal変数だが書き換えてないので宣言しなくてよい。
+    el = {  #キー：押されているキーkey/値：移動量リスト
+        ""     :[0, 0], 
+        "Up"   :[0, -20],
+        "Down" :[0, +20],
+        "Left" :[-20, 0],
+        "Right":[+20, 0],
+        }
+    cx, cy = cx+el[key][0], cy+el[key][1]
+# if文の場合
+    #if key == "Up": cy -= 20
+    #if key == "Down": cy += 20
+    #if key == "Left": cx -= 20
+    #if key == "Right": cx -= 20
+    canvas.coords("tori", cx, cy)
+    root.after(50,main_proc)
+
+
 if __name__ == "__main__":
     root = tk.Tk() #ウィンドウを作成
     root.title("迷えるこうかとん")
@@ -23,4 +42,6 @@ if __name__ == "__main__":
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    
+    main_proc()
     root.mainloop()
