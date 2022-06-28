6955127ad34c6a73dfe600d0ff260d0748b938a0
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-06-28 14:29:58 +0900

    4番まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,5 +3,12 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk() #ウィンドウを作成
     root.title("迷えるこうかとん")
-    root.geometry("1500x900")
+    
+    canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas.pack()
+
+    tori = tk.PhotoImage(file="fig/2.png")
+    cx, cy= 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
+
     root.mainloop()
