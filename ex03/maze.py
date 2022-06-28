69e14b615e0e045874f2550b46e0ca723131aac2
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-06-28 14:38:29 +0900

    6番まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,14 @@
+from calendar import leapdays
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym #keysym→押されたキーの値を取得
+
+def key_up(event):
+    global key
+    key = ""
+    
 if __name__ == "__main__":
     root = tk.Tk() #ウィンドウを作成
     root.title("迷えるこうかとん")
@@ -11,4 +20,7 @@ if __name__ == "__main__":
     cx, cy= 300, 400
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
+    key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     root.mainloop()
