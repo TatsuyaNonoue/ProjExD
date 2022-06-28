396945f598e97fe87717fb6f861f2dc5a2f4507d
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-06-28 15:44:04 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,20 +10,25 @@ def key_up(event):
     key = ""
 
 def main_proc():    #()の中にeventを入れるのはバインドするときのみ。
-    global cx, cy #keyはglobal変数だが書き換えてないので宣言しなくてよい。
+    global cx, cy, mx, my #keyはglobal変数だが書き換えてないので宣言しなくてよい。
     el = {  #キー：押されているキーkey/値：移動量リスト
-        ""     :[0, 0], 
-        "Up"   :[0, -20],
-        "Down" :[0, +20],
-        "Left" :[-20, 0],
-        "Right":[+20, 0],
-        }
-    cx, cy = cx+el[key][0], cy+el[key][1]
-# if文の場合
-    #if key == "Up": cy -= 20
-    #if key == "Down": cy += 20
-    #if key == "Left": cx -= 20
-    #if key == "Right": cx -= 20
+         "Up"   :[0, -1],
+         "Down" :[0, +1],
+         "Left" :[-1, 0],
+         "Right":[+1, 0],
+         }
+    #cx, cy = cx+el[key][0], cy+el[key][1]
+    try:
+        if maze_bg[my+el[key][1]][mx+el[key][0]] == 0: #もし移動先が床なら
+            my, mx = my+el[key][1], mx + el[key][0]
+    except:
+        pass
+    # if文の場合
+    #if key == "Up": my -= 1
+    #if key == "Down": my += 1
+    #if key == "Left": mx -= 1
+    #if key == "Right": mx += 1
+    cx, cy= mx*100+50, my*100+50
     canvas.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -37,7 +42,8 @@ if __name__ == "__main__":
     mm.show_maze(canvas, maze_bg) #canvasにmaze_bgを貼る。
 
     tori = tk.PhotoImage(file="fig/2.png")
-    cx, cy= 300, 400
+    mx, my = 1, 1
+    cx, cy= mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
