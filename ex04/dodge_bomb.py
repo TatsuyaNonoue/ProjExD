dd96ffa6407c14f912977ba6820ccb10b9dedadb
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-07-05 14:23:46 +0900

    第3問終了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,17 +4,22 @@ import sys
 def main():
     clock = pg.time.Clock()          #時間計測用オブジェクト
 
-    pg.display.set_caption("逃げろこうかとん") #タイトルバーに文字表示
-    screen = pg.display.set_mode((1600,900))         #800x600のウィンドウ作成
+    pg.display.set_caption("逃げろこうかとん")          #タイトルバーに文字表示
+    screen = pg.display.set_mode((1600,900))           #800x600のウィンドウ作成
     screen_rect = screen.get_rect()
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
     bgimg_rect = bgimg_sfc.get_rect()
     screen.blit(bgimg_sfc, bgimg_rect)
-#    pg.display.update() #確認してみる。
-#    clock.tick(0.5)     #2秒間だけ表示
+
+    koukaton = pg.image.load("fig/6.png")
+    koukaton = pg.transform.rotozoom(koukaton, 0, 2.0) #サイズ変更
+    koukaton_rect = koukaton.get_rect()                #Rect
+    koukaton_rect.center = 900, 400
+    
 
     while True:
         screen.blit(bgimg_sfc, bgimg_rect)
+        screen.blit(koukaton, koukaton_rect)
         #練習2
         for event in pg.event.get():
             if event.type == pg.QUIT: return
