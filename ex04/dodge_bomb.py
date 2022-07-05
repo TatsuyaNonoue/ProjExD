82c0053d804424195e443df5f1acc72f3839a628
nonotatsu <c0a21102f6@edu.teu.ac.jp>
2022-07-05 14:50:31 +0900

    第5問終了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()          #時間計測用オブジェクト
@@ -11,15 +12,24 @@ def main():
     bgimg_rect = bgimg_sfc.get_rect()
     screen.blit(bgimg_sfc, bgimg_rect)
 
+    #練習3 こうかとん
     koukaton = pg.image.load("fig/6.png")
     koukaton = pg.transform.rotozoom(koukaton, 0, 2.0) #サイズ変更
     koukaton_rect = koukaton.get_rect()                #Rect
     koukaton_rect.center = 900, 400
-    
+
+    #練習5 爆弾
+    bmimg = pg.Surface((20,20))
+    bmimg.set_colorkey((0, 0, 0))
+    pg.draw.circle(bmimg, (255, 0, 0), (10, 10), 10)
+    bmimg_rect = bmimg.get_rect()
+    bmimg_rect.centerx = random.randint(0, screen_rect.width)
+    bmimg_rect.centery = random.randint(0, screen_rect.height)
 
     while True:
         screen.blit(bgimg_sfc, bgimg_rect)
-        #screen.blit(koukaton, koukaton_rect)
+        screen.blit(koukaton, koukaton_rect)
+        screen.blit(bmimg, bmimg_rect)
         #練習2
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -34,6 +44,8 @@ def main():
         if key_states[pg.K_RIGHT] == True: koukaton_rect.centerx += 1
         screen.blit(koukaton, koukaton_rect)
         
+        #練習5 爆弾
+        screen.blit(bmimg, bmimg_rect)
             
 
 if __name__ == "__main__":
